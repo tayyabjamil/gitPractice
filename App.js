4de9092a2b30c201fe31edd1a7c5b8f9320c59d7
tayyab jamil <50493469+tayyabjamil@users.noreply.github.com>
2022-11-23 23:14:34 +0500

    testing 2

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default class App extends Component {
         <Text>Feature day 2</Text>
         <Text>Feature day 3</Text>
         <Text>Feature Testing</Text>
+        <Text>Feature Testing 2</Text>
    
       </View>
     )
