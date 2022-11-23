c4f0fb4818bb0a6df672797f5fdde34560dc053c
tayyab jamil <50493469+tayyabjamil@users.noreply.github.com>
2022-11-23 23:10:21 +0500

    feature 3

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
         <Text>Main day 1</Text>
         <Text>Feature day 1</Text>
         <Text>Feature day 2</Text>
+        <Text>Feature day 3</Text>
    
       </View>
     )
